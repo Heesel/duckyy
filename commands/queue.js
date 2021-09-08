4d5efecdafee20a56d746f86032e7469d260d6b3
alex-zie <alexander.zieschang@gmail.com>
2021-09-08 18:26:07 +0200

    queue

--- commands/queue.js
+++ commands/queue.js
@@ -8,7 +8,7 @@ module.exports = {
         const queue = client.distube.getQueue(message)
         if (!queue) return message.channel.send(`There is nothing playing!`)
         const q = queue.songs.map((song, i) => `${i === 0 ? "Playing:" : `${i}.`} ${song.name} - \`${song.formattedDuration}\``).join("\n")
-        
+
         // const chunks = Discord.Util.splitMessage(queue);
         // const embed = new Discord.MessageEmbed().setTitle(`queue`)
         var result = [];
@@ -31,13 +31,35 @@ module.exports = {
             }
         }
 
+        /*Creating the pagination*/
+        const button1 = new MessageButton()
+                        .setCustomId('previousbtn')
+                        .setLabel('Previous')
+                        .setStyle('DANGER');
+
+        const button2 = new MessageButton()
+                        .setCustomId('nextbtn')
+                        .setLabel('Next')
+                        .setStyle('SUCCESS');
+
+        pages = [];
+        buttons = [button1, button2];
         for(let i=0; i<result.length; i++){
-            console.log(result[i]);
-            
-            message.channel.send(`**Server Queue**\n${result[i]}`)
+            pages.push(new MessageEmbed()
+                .setTitle(""+i+". page")
+                .setDescription(result[i])
+              );
         }
+
+        paginationEmbed(pages, buttons, 12000)
+
+        /*for(let i=0; i<result.length; i++){
+            console.log(result[i]);
+
+            message.channel.send(`**Server Queue**\n${result[i]}`)
+        }*/
         // console.log(res);
-        
+
         // console.log(q);
     }
 }
