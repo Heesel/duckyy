ad88496ec23f4fe04836f23484caab84df10f01d
Heesel <hesselpa@live.nl>
2021-09-10 20:21:49 +0200

    queue

--- commands/queue.js
+++ commands/queue.js
@@ -11,10 +11,18 @@ module.exports = {
         if(queue.songs.length < 2) return message.channel.send(`Currently playing: \`${queue.songs[0].name}\` - \`${queue.songs[0].formattedDuration}\``)
         const q = queue.songs.map((song, i) => `${i === 0 ? "Playing:" : `${i}.`} ${song.name} - \`${song.formattedDuration}\``).join("\n")
 
+        // message.channel.send("**Queue**");
+        // for(let i=0; i<queue.songs.length; i++){
+        // message.channel.send(`${i === 0 ? "" : `${i}.`} ${song.name} - \`${song.formattedDuration}\``);
+        // }
+        // return;
+
+
         var result = [];
         var a = "";
         var i=0;
         var counter = 0;
+
         while(i<q.length){
             if(counter<=10){
                 while(q[i] != '\n' && i<q.length ){
@@ -22,6 +30,10 @@ module.exports = {
                     i++;
                 }
                 a += '\n';
+                if(i<q.length){
+                  result.push(a);
+                  break;
+                }
                 i++;
                 counter++;
             } else {
@@ -45,6 +57,7 @@ module.exports = {
         const pages = [];
         const buttons = [previous, next];
 
+        console.log(result.length);
         for (let i = 0; i < result.length; i++) {
             const pageEmbed = new MessageEmbed();
             pageEmbed
@@ -53,15 +66,7 @@ module.exports = {
             pages.push(pageEmbed);
         }
 
-        paginationEmbed(message, pages, buttons, 250000)
+        paginationEmbed(message, pages, buttons, 100000)
 
-        /*for(let i=0; i<result.length; i++){
-            console.log(result[i]);
-
-            message.channel.send(`**Server Queue**\n${result[i]}`)
-        }*/
-        // console.log(res);
-
-        // console.log(q);
     }
 }
