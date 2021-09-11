d8ac4e01f8f6f50f492e32f834e1c703f021eb79
Heesel <hesselpa@live.nl>
2021-09-11 13:23:44 +0200

    queue rework

--- commands/queue.js
+++ commands/queue.js
@@ -7,66 +7,50 @@ module.exports = {
     aliases: ["q"],
     run: async (client, message, args) => {
         const queue = client.distube.getQueue(message)
-        if (!queue) return message.channel.send(`There is nothing playing!`)
-        if(queue.songs.length < 2) return message.channel.send(`Currently playing: \`${queue.songs[0].name}\` - \`${queue.songs[0].formattedDuration}\``)
-        const q = queue.songs.map((song, i) => `${i === 0 ? "Playing:" : `${i}.`} ${song.name} - \`${song.formattedDuration}\``).join("\n")
 
-        // message.channel.send("**Queue**");
-        // for(let i=0; i<queue.songs.length; i++){
-        // message.channel.send(`${i === 0 ? "" : `${i}.`} ${song.name} - \`${song.formattedDuration}\``);
-        // }
-        // return;
+        if (!queue || !queue.songs || queue.songs.length === 0) return message.channel.send('There is nothing playing!')
+        
+        if (queue.songs.length === 1) return message.channel.send(`Currently playing: \`${queue.songs[0].name}\` - \`${queue.songs[0].formattedDuration}\``)
 
+        const queueSongs = queue.songs.map((song, index) => `${index === 0 ? "Playing:" : `${index}.`} ${song.name} - \`${song.formattedDuration}\``);
 
-        var result = [];
-        var a = "";
-        var i=0;
-        var counter = 0;
+        const pages = [];
+        //iterate queueSongs per 10 songs (pagesize = 10)
+        for (let i = 0; i < queueSongs.length; i += 10) {
+            let pageContent = '';
+            //grab items for current page
+            for (let n = i; n < (i+ 10) && n < queueSongs.length; n++) {
+                if (pageContent !== '') pageContent += '\n';
 
-        while(i<q.length){
-            if(counter<=10){
-                while(q[i] != '\n' && i<q.length ){
-                    a += q[i];
-                    i++;
-                }
-                a += '\n';
-                if(i<q.length){
-                  result.push(a);
-                  break;
-                }
-                i++;
-                counter++;
-            } else {
-                counter = 0;
-                result.push(a);
-                a = "";
+                pageContent += queueSongs[n];
             }
+
+            pages.push(pageContent);
         }
 
         /*Creating the pagination*/
         const previous = new MessageButton()
-                        .setCustomId('previousbtn')
-                        .setLabel('Previous')
-                        .setStyle('DANGER');
+            .setCustomId('previousbtn')
+            .setLabel('Previous')
+            .setStyle('DANGER');
 
         const next = new MessageButton()
-                        .setCustomId('nextbtn')
-                        .setLabel('Next')
-                        .setStyle('SUCCESS');
+            .setCustomId('nextbtn')
+            .setLabel('Next')
+            .setStyle('SUCCESS');
 
-        const pages = [];
         const buttons = [previous, next];
 
-        console.log(result.length);
-        for (let i = 0; i < result.length; i++) {
+        console.log('pages: ' + pages.length);
+        const embeds = pages.map((page, index) => {
             const pageEmbed = new MessageEmbed();
             pageEmbed
-            .setTitle(""+(i+1)+". Page")
-            .setDescription(result[i]);
-            pages.push(pageEmbed);
-        }
+                .setTitle(`Page ${index + 1}`)
+                .setDescription(page);
+            
+            return pageEmbed;
+        });
 
-        paginationEmbed(message, pages, buttons, 100000)
-
+        paginationEmbed(message, embeds, buttons, 100000)
     }
 }
