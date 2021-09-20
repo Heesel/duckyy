ba9dbb8130bbcede788e817ff41f9552e636ec62
Heesel <hesselpa@live.nl>
2021-09-20 19:54:57 +0200

    fixed crash when -skip was used with 1 song in the queue

--- commands/skip.js
+++ commands/skip.js
@@ -4,6 +4,8 @@ module.exports = {
     run: async (client, message, args) => {
         const queue = client.distube.getQueue(message)
         if (!queue) return message.channel.send(`There is nothing in the queue right now!`)
+        if (queue.songs.length === 1) return message.channel.send("There is no song to skip.");
+        console.log('Log: ' + queue.songs.length);
         try {
             client.distube.skip(message)
             message.channel.send(`Skipped! Now playing:\n${queue.songs[0].name}`)
