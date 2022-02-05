c47574877464ef056d820051bd50cfc535fdb766
Heesel <hesselpa@live.nl>
2022-02-05 13:38:50 +0100

    Fixed disconnect command

--- commands/stop.js
+++ commands/stop.js
@@ -1,11 +1,18 @@
+const voice = require('@discordjs/voice');
+
 module.exports = {
     name: "stop",
     aliases: ["disconnect", "leave", "dc"],
     inVoiceChannel: true,
     run: async (client, message, args) => {
         const queue = client.player.getQueue(message.guild.id);
-        if (!queue) return message.channel.send(`There is nothing in the queue right now!`)
+        if (!queue) {
+            message.channel.send(`There is nothing in the queue right now!`)
+            voice.getVoiceConnection(message.guild.id).disconnect();
+            return;
+        } 
         queue.stop();
-        message.channel.send(`Stopped!`)
+        message.channel.send('The queue has been ended. Cya :wave:')
+        voice.getVoiceConnection(message.guild.id).disconnect();
     }
 }
